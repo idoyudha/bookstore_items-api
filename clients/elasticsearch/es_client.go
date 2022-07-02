85866ff9a48972dcbecae4ea70fbc483d1049f82
idoyudha <idowidya.yudhatama@gmail.com>
2022-07-02 11:11:39 +0700

    add indexing func

--- clients/elasticsearch/es_client.go
+++ clients/elasticsearch/es_client.go
@@ -77,11 +77,26 @@ func (c *esClient) Index(index string, doc interface{}) (*esapi.Response, error)
 			fmt.Sprintf("error when trying to index document in index %s", index), err)
 		return nil, err
 	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		log.Printf("[%s] Error indexing document", res.Status())
+	} else {
+		var r map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+			log.Printf("Error parsing the response body: %s", err)
+		} else {
+			log.Printf("[%s] %s; version=%d", res.Status(), r["result"], int(r["_version"].(float64)))
+		}
+	}
 	return res, nil
 }
 
 func (c *esClient) Get(index string, id string) (*esapi.Response, error) {
-	// ctx := context.Background()
-	// result, err := c.client.Get()
-	return nil, nil
+	result, err := c.client.Get(index, id)
+	if err != nil {
+		log.Println("Error get id")
+		return nil, err
+	}
+	return (*esapi.Response)(result), err
 }
